14b7d398e33e538909da59b85e4934580c4d985c
Nata <natamoskvina@mail.ru>
2020-02-09 18:24:41 +0000

    Fixed 4 locators

--- src/test/java/Test_Git.java
+++ src/test/java/Test_Git.java
@@ -25,12 +25,13 @@ public class Test_Git {
         driver.findElement(By.name("search")).sendKeys("java шилдт");
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
+        
         //кнопка в корзину
-        driver.findElement(By.xpath("//div[@class='a2u6']/div[1]//button[@qa-id='tile-buy-button']")).click(); // Fixed the locator here (Locator for the first element in the list)
-        driver.findElement(By.cssSelector(".f-caption--bold")).click(); // Fixed the locator here (Replaced dynamic svg on the css class)
+        driver.findElement(By.xpath("//div[contains(@class, 'widget-search-result-container')]//*[@qa-id='tile-buy-button']")).click(); // Fixed the locator here (Locator for the first element in the list)
+        driver.findElement(By.cssSelector("[data-widget='cart'] .f-caption--bold")).click(); // Fixed the locator here (Replaced dynamic svg on the css class)
         //кнопка удалить из корзины
-        driver.findElement(By.xpath("//div[@class='column md-8']//span[2]")).click(); // Fixed the locator here
-        driver.findElement(By.xpath("//div[@class='ui-b7 h4']//div[@class='ui-aa5']")).click(); // Fixed the locator here (Fixed this locator too)
+        driver.findElement(By.xpath("//div[@class='a5g2']//*[contains(text(), 'Удалить')]")).click(); // Fixed the locator here
+        driver.findElement(By.xpath("//div[@class='modal-content']//button")).click(); // Fixed the locator here (Fixed this locator too)
     }
 
     @After
