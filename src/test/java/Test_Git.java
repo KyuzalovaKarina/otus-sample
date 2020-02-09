f7b4619686f6f1742a8a44f7544ec6cb97bb788c
tanyaryzhova <tanyaryzhova@users.noreply.github.com>
2020-02-09 16:27:53 +0100

    test fix

--- src/test/java/Test_Git.java
+++ src/test/java/Test_Git.java
@@ -1,4 +1,6 @@
 import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
@@ -12,7 +14,7 @@ public class Test_Git {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -26,11 +28,16 @@ public class Test_Git {
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
         //кнопка в корзину
-        driver.findElement(By.xpath("//div[@class='a2u6']/div[1]//button[@qa-id='tile-buy-button']")).click(); // Fixed the locator here (Locator for the first element in the list)
-        driver.findElement(By.cssSelector(".f-caption--bold")).click(); // Fixed the locator here (Replaced dynamic svg on the css class)
+        WebElement addToCart = driver.findElement(By.cssSelector(".a8x6>.a8w8>div>div>button"));
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", addToCart);
+        driver.findElement(By.cssSelector("a[href='/cart']")).click();
         //кнопка удалить из корзины
-        driver.findElement(By.xpath("//div[@class='column md-8']//span[2]")).click(); // Fixed the locator here
-        driver.findElement(By.xpath("//div[@class='ui-b7 h4']//div[@class='ui-aa5']")).click(); // Fixed the locator here (Fixed this locator too)
+        WebElement removeFromCart = driver.findElement(By.cssSelector(".a5h5>span:nth-child(2)"));
+        executor.executeScript("arguments[0].click();", removeFromCart);
+        driver.findElement(By.cssSelector("[class='modal-container'] button>div")).click();
+        WebElement cartIsEmpty = driver.findElement(By.cssSelector("h1"));
+        assertTrue(cartIsEmpty.isDisplayed());
     }
 
     @After
