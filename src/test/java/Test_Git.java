7627042217b371ab99bc761c2207ad6533d4169e
Shitov Sergei <p64shitovsergei@gmail.com>
2020-02-09 12:33:11 +0400

    Fixed the locators in Ozon test from 09022020 - Clarified the first element in the list - Fixed CSS locator for the basket - Fixed XPath locator for the "delete" button in popup

--- src/test/java/Test_Git.java
+++ src/test/java/Test_Git.java
@@ -26,11 +26,11 @@ public class Test_Git {
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
         driver.findElement(By.cssSelector("svg.ui-ai2")).click();
         //кнопка в корзину
-        driver.findElement(By.xpath("//button[@qa-id='tile-buy-button']")).click(); // Fixed the locator here
-        driver.findElement(By.cssSelector("svg.a3e8")).click(); // Fixed the locator here
+        driver.findElement(By.xpath("//div[@class='a2u6']/div[1]//button[@qa-id='tile-buy-button']")).click(); // Fixed the locator here (Locator for the first element in the list)
+        driver.findElement(By.cssSelector(".f-caption--bold")).click(); // Fixed the locator here (Replaced dynamic svg on the css class)
         //кнопка удалить из корзины
         driver.findElement(By.xpath("//div[@class='column md-8']//span[2]")).click(); // Fixed the locator here
-        driver.findElement(By.xpath("//div[@class='ui-b7 g3']")).click(); // Fixed the locator here
+        driver.findElement(By.xpath("//div[@class='ui-b7 h4']//div[@class='ui-aa5']")).click(); // Fixed the locator here (Fixed this locator too)
     }
 
     @After
